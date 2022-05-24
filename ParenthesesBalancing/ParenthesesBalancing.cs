870672792c3989407f59abbc1122f0c1063c3945
Nikita Luksha <nikita.luksha@softensity.com>
2022-05-24 14:17:01 +0300

    Task1

--- ParenthesesBalancing/ParenthesesBalancing.cs
+++ ParenthesesBalancing/ParenthesesBalancing.cs
@@ -2,9 +2,32 @@
 {
     public class ParenthesesBalancing
     {
+        private const char OPEN_PARENTHESIS = '(';
+        private const char CLOSE_PARENTHESIS = ')';
+
         public bool IsBalanced(string input)
         {
-            throw new NotImplementedException("implement me");
+            int openParenthesisCount = 0;
+
+            foreach (var element in input)
+            {
+                if (element == OPEN_PARENTHESIS)
+                {
+                    openParenthesisCount++;
+                }
+                else if (element == CLOSE_PARENTHESIS)
+                {
+                    if (openParenthesisCount == 0)
+                    {
+                        return false;
+                    } else
+                    {
+                        openParenthesisCount--;
+                    }
+                }
+            }
+
+            return openParenthesisCount == 0;
         }
     }
 }
