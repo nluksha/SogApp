0d8c99acb0c485aaa8f77fe7c239e02a5e401425
Nikita Luksha <nikita.luksha@softensity.com>
2022-05-24 16:20:20 +0300

    Task3

--- Reverse/CharReverse.cs
+++ Reverse/CharReverse.cs
@@ -1,10 +1,46 @@
-﻿namespace Reverse
+﻿using System.Text;
+
+namespace Reverse
 {
     public class CharReverse
     {
+        private HashSet<char> delimiters = new HashSet<char>() { ' ', ',', '.' };
+
         public string Reverse(string input)
         {
-            throw new NotImplementedException("to be implemented");
+            var output = new StringBuilder();
+            StringBuilder? wordBuffer = null;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var currentChar = input[i];
+
+                if (delimiters.Contains(currentChar))
+                {
+                    if (wordBuffer != null)
+                    {
+                        output.Append(wordBuffer);
+                        wordBuffer = null;
+                    }
+
+                    output.Append(currentChar);
+                } else
+                {
+                    if (wordBuffer == null)
+                    {
+                        wordBuffer = new StringBuilder();
+                    }
+
+                    wordBuffer.Insert(0, currentChar);
+                }
+            }
+
+            if (wordBuffer != null)
+            {
+                output.Append(wordBuffer);
+            }
+
+            return output.ToString(); ;
         }
     }
 }
